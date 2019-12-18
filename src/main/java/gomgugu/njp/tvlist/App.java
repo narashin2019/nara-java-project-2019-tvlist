41f5dcd3a9f384988b56f99609c1f286d98d3cd0
gomgugu <narashin547@gmail.com>
2019-12-18 16:03:54 +0900

    프로젝트 초기화

---
+++ src/main/java/gomgugu/njp/tvlist/App.java
@@ -0,0 +1,10 @@
+// 프로젝트: 내가 본 드라마(TV Show) 정리 및 공유
+//
+package gomgugu.njp.tvlist;
+
+public class App {
+
+  public static void main(String[] args) {
+    System.out.println("드라마 공유 사이트에 오신 것을 환영해요.");
+  }
+}
