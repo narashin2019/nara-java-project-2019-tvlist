45764c3ba75633a98af0c6651a7fc4672e6ba720
gomgugu <narashin547@gmail.com>
2019-12-18 16:35:24 +0900

    222출력삭제

--- src/main/java/gomgugu/njp/tvlist/App.java
+++ src/main/java/gomgugu/njp/tvlist/App.java
@@ -5,8 +5,7 @@ package gomgugu.njp.tvlist;
 public class App {
 
   public static void main(String[] args) {
-    System.out.println("드라마 공유 사이트에 오신 것을 환영해요.");
+    System.out.println("드라마목록 공유 사이트에 오신 것을 환영해요.");
     System.out.println("111");
-    System.out.println("222");
   }
 }
