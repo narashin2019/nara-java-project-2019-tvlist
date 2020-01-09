bd2f5b0a00fb5470fdd453c5cd8dfb29022a1c39
gomgugu <narashin547@gmail.com>
2020-01-09 18:02:07 +0900

    200109

--- src/main/java/gomgugu/njp/util/ArrayList.java
+++ src/main/java/gomgugu/njp/util/ArrayList.java
@@ -3,139 +3,175 @@ package gomgugu.njp.util;
 import java.util.Arrays;
 
 public class ArrayList<E> {
-  //ArrayList에서 다룰 <클래스이름>을 받는변수, E = 변수명이나 일반적인 변수명아님
-  //E만약 E자리에 다른 클래스 이름이 온다면 E를 쓰던 자리는 다 바꾸어야 한다.
 
-  static final int DEFAULT_CAPACITY = 3;
+  private static final int DEFAULT_CAPACITY = 2;
 
-  Object[] list;
-  int size = 0; // 뉴 했을떄 만들어지는 메모리의 설계도
+  //뉴 했을떄 만들어지는 메모리의 설계도 = 인스턴스 필드. 초기화 필요없다.
+  Object[] elementData;
+  int size;
 
-
+  //생성자
   public ArrayList() {
-    this.list = new Object[DEFAULT_CAPACITY];
+    this.elementData = new Object[DEFAULT_CAPACITY];
   }
 
-
-  public ArrayList(int capacity) {
-    if(capacity < DEFAULT_CAPACITY || capacity >10000)
-      this.list = new Object[DEFAULT_CAPACITY];
+  //생성자
+  public ArrayList(int initialCapacity) {
+    if(initialCapacity < DEFAULT_CAPACITY)
+      this.elementData = new Object[DEFAULT_CAPACITY];
     else
-      this.list = new Object[capacity];
+      this.elementData = new Object[initialCapacity];
   }
 
 
-  @SuppressWarnings("unchecked")
-  public E[] toArray(E[] arr) { // 제네릭은  E[] arr = new = E[100]; 이거 안됨  배열을 뉴로 못만듬
 
-    if (arr.length < this.size) {
-      return (E[]) Arrays.copyOf(this.list, this.size, arr.getClass());
-    }
-
-    System.arraycopy(this.list, 0, arr, 0, this.size); 
-    //this.list를 0번방부터 arr에 0번방부터 this.size만큼 복사한다
-
-    // 위는 클래스메서드 = 스태틱메서드. 주어진 파라미터 가지고 일을하는 메서드는 스태틱메서드. 특정 인스턴스변수 쓰지않음.
-    // 위의 arraycopy()는 다음 코드와 같다.
-    // for (int i = 0; i <arr.length; i++) {
-    //   arr[i] = (E) this.list[i];
-    // }
-
-    return arr;
-  }
-
-
-  public void add(E obj) {
-    if(this.size == this.list.length) {
-      int oldCapacity = this.list.length;
-      int newCapacity = oldCapacity + (oldCapacity >> 1);
-      this.list = Arrays.copyOf(this.list, newCapacity);
-    }
-    this.list[this.size++] = obj;
-  }
 
 
   //목록의 특정 위치에 저장된 항목을 꺼내주는 get()
   @SuppressWarnings("unchecked")
   public E get(int index) {
-    if (index >= 0 && index < this.size) {
-      return (E) this.list[index]; //타입캐스팅
-    } else {
+    if (index < 0 || index >= this.size) {
       return null;
+    } else {
+      return (E) this.elementData[index]; 
+      //타입캐스팅(E) 실행할 때 저장되는 값의 리턴타입이 결정된다. 
+      //스트링이면 스트링인지 아닌지 컴파일러가 다 검사함.
     }
   }
 
 
-  //목록의 특정 위치에 저장된 항목을 바꾸는 set()
+  //목록의 특정 위치에 저장된 항목을 바꾸는 set() // 리턴값은 예전 항목이다. // 원치않으면 리턴 안받으면 된다.
   @SuppressWarnings("unchecked")
-  public E set(int index, E obj) {
-    if (index < 0 || index >= this.size)
+  public E set(int index, E e) {
+    if (index < 0 || index >= this.size) {
       return null;
-
-    E old = (E) this.list[index];
-    this.list[index] = obj;
-
-    return old;
+    }
+    // 바꾸기전 항목을 따로 보관해 둔다. 필요할 수 있다. 
+    E oldValue = (E) this.elementData[index];
+    this.elementData[index] = e;
+    return oldValue;
   }
 
 
-  //목록의 특정 위치에 저장된 항목을 삭제하는 remove()
+  //목록의 특정 위치에 저장된 항목을 삭제하는 remove() // 리턴값은 예전 항목이다. // 원치않으면 리턴 안받으면 된다.
   @SuppressWarnings("unchecked")
   public E remove(int index) {
-    if (index < 0 || index >= this.size)
+    if (index < 0 || index >= this.size) {
       return null;
-
-    //삭제할 항목을 따로 보관해 둔다. 왜...?
-    E old = (E) this.list[index];
-
-    for (int i = index + 1; i < this.size; i++) {
-      this.list[i - 1] = this.list[i];
     }
-
+    
+    //삭제할 항목을 따로 보관해 둔다. 필요할 수 있다.
+    E oldValue = (E) this.elementData[index];
+    
+    System.arraycopy(this.elementData, index + 1, 
+        this.elementData, index, this.size - (index + 1));
+    /*
+    for (int i = index + 1; i < this.size; i++) {
+      this.elementData[i - 1] = this.elementData[i];
+    }
+    */
+    
     this.size--;
-
-    this.list[this.size] = null;
-
-    // 삭제한 항목을 리턴한다. 왜????
-    return old;
-
+    return oldValue;
   }
-
-
+  
+  
   public int size() {
     return this.size; //this는 생략가능. this변수에 들어있는 size
   }
-
   
-  public static void main(String[] args) {
-    ArrayList<String> list = new ArrayList<>();
-    list.add("aaaa");
-    list.add("bbbb");
-    list.add("cccc");
-    list.add("dddd");
-    list.add("eeee");
-    list.add("ffff");
-    
-    //list.remove(6);
-//    list.set(0, "0000");
-//    list.set(3, "3333");
-//    list.set(5, "5555");
-//    list.set(-1, "ㅋㅋㅋㅋ");
-//    list.set(6, "^^^^^^");
-    
-    
-    String[] arr = list.toArray(new String[] {});
-    for (String s : arr) {
-      System.out.println(s);
+  
+  public Object[] toArray() {
+    return Arrays.copyOf(this.elementData, this.size);
+    /*
+    Object[] arr = new Object[this.size];
+    for (int i = 0; i < this.size; i++) {
+      arr[i] = this.elementData[i];
     }
-    
-    
+    return arr;
+    */
   }
   
+  
+  @SuppressWarnings("unchecked")
+  public E[] toArray(E[] arr) { 
+ // 제네릭은  E[] arr = new E[100]; 이거 안됨  배열을 뉴로 못만듬
+    
+    if (arr.length < this.size) {
+      // 파라미터로 받은 배열이 작을 때는 현재 사이즈 크기의 새 배열을 만들어 리턴
+      return (E[]) Arrays.copyOf(this.elementData, this.size, arr.getClass()); //arr.getClass() 클래스타입이 무엇인지 정보 가져옴
+    }
+    // 파라미터로 받은 배열 넉넉할 때는 파라미터로 받은 배열을 그대로 리턴. 
+    System.arraycopy(this.elementData, 0, arr, 0, this.size); 
+    //this.elementData를 0번방부터 arr에 0번방부터 this.size만큼 복사한다
+    return arr;
+  }
+  
+
+  public void add(E e) {
+    if(this.size == this.elementData.length) {
+      int oldSize = this.elementData.length;
+      int newSize = oldSize + (oldSize >> 1);
+
+      this.elementData = Arrays.copyOf(this.elementData, newSize);
+      /*
+      Object[] newArr = new Object[newSize];
+       //새 배열에 기존 값을 반복문으로 넣는다
+      for (int i = 0; i < this.size; i++) {
+        newArr[i] = this.elementData[i];
+      }
+      this.elementData = newArr;
+      // 새 배열의 주소를 엘레멘트데이터에 준다
+       */
+
+    }
+    this.elementData[this.size++] = e;
+  }
+  
+  
+  public void add(int index, E value) {
+    if (index < 0 || index >= this.size)
+      return;
+
+    if (this.size == this.elementData.length) {
+      int oldSize = this.elementData.length;
+      int newSize = oldSize + (oldSize >> 1);
+      
+      this.elementData = Arrays.copyOf(this.elementData, newSize);
+    }
+
+    for (int i = size - 1; i >= index; i--)
+      this.elementData[i + 1] = this.elementData[i];
+
+    this.elementData[index] = value;
+    this.size++;
+  }
+  
+  
+//  public static void main(String[] args) {
+//    ArrayList<String> list = new ArrayList<>();
+//    list.add("aaaa");
+//    list.add("bbbb");
+//    list.add("cccc");
+//    list.add("dddd");
+//    list.add("eeee");
+//    list.add("ffff");
+//
+//    //list.remove(6);
+//    //    list.set(0, "0000");
+//    //    list.set(3, "3333");
+//    //    list.set(5, "5555");
+//    //    list.set(-1, "ㅋㅋㅋㅋ");
+//    //    list.set(6, "^^^^^^");
+//
+//
+//    String[] arr = list.toArray(new String[] {});
+//    for (String s : arr) {
+//      System.out.println(s);
+//    }
+//  }
+
 }
 
-//copyOf () 메서드야
-//arrayType에 지정된 배열을 size만큼 만들어라.
-//그리고 List배열에 저장죈 주소를 새로 만든 배열에 복사하라.
-//마지막으로 새로만든 배열의 주소를 리턴한다,
+
 
